e73a4aa165f50020e7bad3ac12224e9b0b5bdd38
zaid <zaidalshreef@gmail.com>
2021-10-04 20:21:12 +0300

    new

--- test_main.py
+++ test_main.py
@@ -6,11 +6,11 @@ import json
 import pytest
 
 import main
+SECRET = os.environ.get('SECRET')
+TOKEN = os.environ.get('TOKEN')
+EMAIL = os.environ.get('EMAIL')
+PASSWORD = os.environ.get('PASSWORD')
 
-SECRET = 'TestSecret'
-TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
-EMAIL = 'wolf@thedoor.com'
-PASSWORD = 'huff-puff'
 
 @pytest.fixture
 def client():
